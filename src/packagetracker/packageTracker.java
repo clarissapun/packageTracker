b406fe1da89fc43366203ec9dd628d3ba4f75427
Liz <ejm5763@gmail.com>
2020-12-07 23:25:55 -0500

    push code to fix error

--- src/packagetracker/packageTracker.java
+++ src/packagetracker/packageTracker.java
@@ -20,7 +20,7 @@ public class packageTracker extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/view/PackageListView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/ProfileView.fxml"));
         
         Scene scene = new Scene(root);
         
